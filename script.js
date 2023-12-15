2da71c11562f137b15c96d763f51ede36883d61c
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-15 12:57:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	
+	let str = "";
+	if(s1.length===0 || s2.length===0){
+		return str;
+	}
+	else{
+		s1 = s1.split(" ").join("");
+		s2 = s2.split(" ").join("");
+		
+		return str+s1.charAt(0)+s2.charAt(0);
+	}
 }
 
 // Do not change the code below.
